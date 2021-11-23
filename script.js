cb83445436b29b0a51e708a54ae2a0d75fd41e96
Veda Simply Coding <90026881+vedasimplycoding@users.noreply.github.com>
2021-11-23 08:13:32 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,30 +2,16 @@ document.addEventListener("contextmenu", function(event){
 event.preventDefault();
 alert('Right Click is Disabled');    
 }, false);
-document.addEventListener('keydown', function() {
-  if (event.keyCode == 123) {
-    alert("This function has been disabled to prevent you from stealing my code!");
-    return false;
-  } else if (event.ctrlKey && event.shiftKey && event.keyCode == 73) {
-    alert("This function has been disabled to prevent you from stealing my code!");
-    return false;
-  } else if (event.ctrlKey && event.keyCode == 85) {
-    alert("This function has been disabled to prevent you from stealing my code!");
-    return false;
-  }
-}, false);
+/* To Disable Inspect Element */
+$(document).bind("contextmenu",function(e) {
+ e.preventDefault();
+});
 
-if (document.addEventListener) {
-  document.addEventListener('contextmenu', function(e) {
-    alert("This function has been disabled to prevent you from stealing my code!");
-    e.preventDefault();
-  }, false);
-} else {
-  document.attachEvent('oncontextmenu', function() {
-    alert("This function has been disabled to prevent you from stealing my code!");
-    window.event.returnValue = false;
-  });
-}
+$(document).keydown(function(e){
+    if(e.which === 123){
+       return false;
+    }
+});
 VanillaTilt.init(document.querySelector('.front'), {
   reverse: true,
   glare: true,
