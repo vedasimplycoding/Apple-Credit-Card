d63bc097a582d25f045dd4cea9311b5872c5ebbc
Veda Simply Coding <90026881+vedasimplycoding@users.noreply.github.com>
2021-11-23 08:15:29 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,35 @@
-document.addEventListener("contextmenu", function(event){
-event.preventDefault();
-alert('Right Click is Disabled');    
-}, false);
-/* To Disable Inspect Element */
+document.onkeydown = function(e) {
+if(event.keyCode == 123) {
+return false;
+}
+if(e.ctrlKey && e.keyCode == 'E'.charCodeAt(0)){
+return false;
+}
+if(e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)){
+return false;
+}
+if(e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)){
+return false;
+}
+if(e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)){
+return false;
+}
+if(e.ctrlKey && e.keyCode == 'S'.charCodeAt(0)){
+return false;
+}
+if(e.ctrlKey && e.keyCode == 'H'.charCodeAt(0)){
+return false;
+}
+if(e.ctrlKey && e.keyCode == 'A'.charCodeAt(0)){
+return false;
+}
+if(e.ctrlKey && e.keyCode == 'F'.charCodeAt(0)){
+return false;
+}
+if(e.ctrlKey && e.keyCode == 'E'.charCodeAt(0)){
+return false;
+}
+}
 $(document).bind("contextmenu",function(e) {
  e.preventDefault();
 });
@@ -12,6 +39,10 @@ $(document).keydown(function(e){
        return false;
     }
 });
+document.addEventListener("contextmenu", function(event){
+event.preventDefault();
+alert('Right Click is Disabled');    
+}, false);
 VanillaTilt.init(document.querySelector('.front'), {
   reverse: true,
   glare: true,
