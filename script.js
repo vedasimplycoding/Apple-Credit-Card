c29e2bbbf119bd3fa9b9928850b4240852c94a56
Veda Simply Coding <90026881+vedasimplycoding@users.noreply.github.com>
2021-11-23 08:11:46 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,30 @@ document.addEventListener("contextmenu", function(event){
 event.preventDefault();
 alert('Right Click is Disabled');    
 }, false);
+document.addEventListener('keydown', function() {
+  if (event.keyCode == 123) {
+    alert("This function has been disabled to prevent you from stealing my code!");
+    return false;
+  } else if (event.ctrlKey && event.shiftKey && event.keyCode == 73) {
+    alert("This function has been disabled to prevent you from stealing my code!");
+    return false;
+  } else if (event.ctrlKey && event.keyCode == 85) {
+    alert("This function has been disabled to prevent you from stealing my code!");
+    return false;
+  }
+}, false);
+
+if (document.addEventListener) {
+  document.addEventListener('contextmenu', function(e) {
+    alert("This function has been disabled to prevent you from stealing my code!");
+    e.preventDefault();
+  }, false);
+} else {
+  document.attachEvent('oncontextmenu', function() {
+    alert("This function has been disabled to prevent you from stealing my code!");
+    window.event.returnValue = false;
+  });
+}
 VanillaTilt.init(document.querySelector('.front'), {
   reverse: true,
   glare: true,
